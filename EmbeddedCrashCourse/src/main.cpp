f42d43eeb1fca279ddf50af2d29276a1d48b3208
Emir Taha Kenar <kenar18@itu.edu.tr>
2021-11-06 15:05:36 +0300

    multi_array comm

--- EmbeddedCrashCourse/src/main.cpp
+++ EmbeddedCrashCourse/src/main.cpp
@@ -2,60 +2,53 @@
 
 #include <ros.h>
 #include <std_msgs/Float64MultiArray.h>
-#include <std_msgs/Float64.h>
 
-#define ARRAY_LEN 1
+#define ARRAY_LEN 3
 
 ros::NodeHandle nh;
-//std_msgs::Float64MultiArray command_arr;
-//std_msgs::Float64MultiArray feedback_arr;
 
-std_msgs::Float64 command;
-std_msgs::Float64 feedback;
-float temp_arr[1] = {0.0};
+void processData();
+void commandCallback(const std_msgs::Float64MultiArray &command_msg);
 
-void commandCallback(const std_msgs::Float64 &command_msg);
-void processCommand(const std_msgs::Float64 &command_arr);
+std_msgs::Float64MultiArray feedback_arr;
+std_msgs::Float64MultiArray command_arr;
 
-ros::Publisher feedback_pub("encoder_feedback",&feedback);
-ros::Subscriber<std_msgs::Float64> command_sub("position_command",&commandCallback);
+ros::Publisher feedback_pub("encoder_feedback",&feedback_arr);
+ros::Subscriber<std_msgs::Float64MultiArray> command_sub("command_topic",&commandCallback);
 
 void setup(){
     nh.initNode();
+
+    feedback_arr.data = (float*)malloc(sizeof(float)*ARRAY_LEN);
+    feedback_arr.data_length = ARRAY_LEN;
+
+    command_arr.data = (float*)malloc(sizeof(float)*ARRAY_LEN);
+    command_arr.data_length = ARRAY_LEN;
+
     nh.advertise(feedback_pub);
     nh.subscribe(command_sub);
+
 }
 
 void loop(){
     nh.spinOnce();
-    //feedback_pub.publish(&feedback_arr);
+
+    processData();
     delay(1);
 }
 
+/* --------------- FUNCTION DEFINITIONS --------------- */
 
-void commandCallback(const std_msgs::Float64 &command_msg){
-    /*
+void processData(){
+    for (int i=0; i<ARRAY_LEN;i++){
+        feedback_arr.data[i] = command_arr.data[i]*2;
+    }
+}
+
+void commandCallback(const std_msgs::Float64MultiArray &command_msg){
     for (int i=0; i<ARRAY_LEN; i++){
-        temp_arr[i] = command_msg.data[i];
+        command_arr.data[i] = command_msg.data[i];
     }
     nh.loginfo("New command!");
     feedback_pub.publish(&feedback_arr);
-    */
-   command.data = command_msg.data;
-   nh.loginfo("New command!\n");
-   processCommand(command);
-   feedback_pub.publish(&feedback);
-   
-}
-
-void processCommand(const std_msgs::Float64 &command){
-    /*
-    float arr[1] = {0.0};
-    arr[0] = command_arr.data[0];
-    arr[0] *= 2;
-    feedback_arr.data = arr;
-    */
-   float processed_data = command.data;
-   processed_data *= 2;
-   feedback.data = processed_data;
 }

0c28aa59f143549ecd7abf44f6e89ae898a707c6
Emir Taha Kenar <kenar18@itu.edu.tr>
2021-11-18 15:27:13 +0300

    feedback_com test via uart

--- EmbeddedCrashCourse/src/main.cpp
+++ EmbeddedCrashCourse/src/main.cpp
@@ -1,20 +1,71 @@
 #include <Arduino.h>
 
+#include <SoftwareSerial.h>
+
 #include <ros.h>
 #include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/String.h>
 
-#define ARRAY_LEN 3
+
+
+/* ----------------- MACROS AND CONSTANTS --------------------- */
+#define ARRAY_LEN 1
+
+#define TRUE 1
+#define FALSE 0
+
+
+const float radian_map_coef = 300;
+/* ------------------------------------------------------------ */
+
+
 
 ros::NodeHandle nh;
+
+/* --------------- FUNCTION DECLERATIONS --------------- */
 
 void processData();
 void commandCallback(const std_msgs::Float64MultiArray &command_msg);
 
+void generateStrMsg();
+String floatToStr(float radian_data);
+
+void readDataFromMCU();
+
+
+/* ---------------- GLOBAL VARIABLES -------------------- */
+
 std_msgs::Float64MultiArray feedback_arr;
 std_msgs::Float64MultiArray command_arr;
 
+std_msgs::String debug_str;
+char debug_ch[50];
+
+float commands_to_send[ARRAY_LEN];
+float enc_feedback_to_send[ARRAY_LEN];
+
+String str_angle_command;
+String str_enc_feedback;
+
+int incoming_byte;
+char buf[1];
+char str_arr[7];
+
+int receive_counter = 0;
+int receive_cnt_flag = FALSE;
+
+String incoming_str = "";
+/* ------------------------------------------------------- */
+
+
 ros::Publisher feedback_pub("encoder_feedback",&feedback_arr);
 ros::Subscriber<std_msgs::Float64MultiArray> command_sub("command_topic",&commandCallback);
+
+ros::Publisher debug_pub("debug_topic",&debug_str);
+
+/* ------------- SERIAL CONFIG FOR OTHER STMS -----------*/
+HardwareSerial armSerial(PB11,PB10); // RX, TX
+
 
 void setup(){
     nh.initNode();
@@ -28,16 +79,60 @@ void setup(){
     nh.advertise(feedback_pub);
     nh.subscribe(command_sub);
 
+    nh.advertise(debug_pub);
+
+    // Serial inits
+    armSerial.begin(57600);
+
 }
 
 void loop(){
     nh.spinOnce();
 
-    processData();
-    delay(1);
+    //processData();
+    readDataFromMCU();
+
+    
+
+
+    /*
+    if (armSerial.available() > 0){
+        incoming_byte = armSerial.read();
+        buf[0] = incoming_byte;
+        nh.loginfo(buf);
+    }
+    */
+
+    //feedback_pub.publish(&feedback_arr);
+    
 }
 
 /* --------------- FUNCTION DEFINITIONS --------------- */
+
+void readDataFromMCU(){
+     
+    
+    
+    if (armSerial.available() > 0){
+        incoming_byte = armSerial.read();
+        if (incoming_byte == 'A'){
+            incoming_str = "";
+            incoming_str += (char) incoming_byte;
+            return;
+        }
+        else if (receive_cnt_flag = TRUE && incoming_byte != 'B'){
+            incoming_str += (char) incoming_byte;
+        }
+        else if (incoming_byte == 'B'){
+            incoming_str += (char) incoming_byte;
+            incoming_str.toCharArray(str_arr,7);
+            nh.loginfo(str_arr);
+            incoming_str = "";
+        }   
+
+    }
+}
+
 
 void processData(){
     for (int i=0; i<ARRAY_LEN;i++){
@@ -47,8 +142,50 @@ void processData(){
 
 void commandCallback(const std_msgs::Float64MultiArray &command_msg){
     for (int i=0; i<ARRAY_LEN; i++){
-        command_arr.data[i] = command_msg.data[i];
+        commands_to_send[i] = command_msg.data[i];
     }
     nh.loginfo("New command!");
     feedback_pub.publish(&feedback_arr);
+
+
+    generateStrMsg();
+    str_angle_command.toCharArray(debug_ch,50);
+    debug_str.data = debug_ch;
+    debug_pub.publish(&debug_str);
+    
+    armSerial.println(str_angle_command);
+    nh.loginfo("Sent\n\n");
+}
+
+void generateStrMsg(){
+    str_angle_command = "S";
+
+    for (int i=0; i<ARRAY_LEN; i++){
+        str_angle_command += floatToStr(commands_to_send[i]);
+    }
+
+    str_angle_command += "F";
+
+}
+
+String floatToStr(float radian_data){
+    String mapped_angle;
+    if (radian_data > 0){
+        mapped_angle = "1";
+    }
+    else{
+        mapped_angle = "0";
+    }
+    radian_data = abs(radian_data);
+    int encoded_radian = radian_data * radian_map_coef;
+
+    String encoded_str = String(encoded_radian);
+
+    while (encoded_str.length() < 3){
+        encoded_str = "0" + encoded_str;
+    }
+
+    mapped_angle += encoded_str;
+
+    return mapped_angle;
 }
